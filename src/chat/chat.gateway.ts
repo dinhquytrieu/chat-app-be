1785bd6e69591bf8b0446512398bd7cc10762d66
dinhquytrieu <dqtrieu21@clc.fitus.edu.vn>
2024-11-19 00:26:28 +0700

    first commit

---
+++ src/chat/chat.gateway.ts
@@ -0,0 +1,18 @@
+import {
+  WebSocketGateway,
+  SubscribeMessage,
+  MessageBody,
+  WebSocketServer,
+} from '@nestjs/websockets';
+import { Server } from 'socket.io';
+
+@WebSocketGateway({ cors: true })
+export class ChatGateway {
+  @WebSocketServer()
+  server: Server;
+
+  @SubscribeMessage('message')
+  handleMessage(@MessageBody() data: { user: string; message: string }): void {
+    this.server.emit('message', data);
+  }
+}
